01044d6874a84777c5e77c4141fd9fbc6bb85bd8
qiancheng <qiancheng@chunbo.com>
2015-05-15 10:03:44 +0800

    2

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ var app = module.exports.app = exports.app = express();
 var appbase = path.join(__dirname);
 
 app.use( '/', express.static( path.join(appbase + '/build/'+version+'/')));
-app.use('/static', express.static(__dirname + '/build/'+version+'/'));
+app.use('/static', express.static(appbase + '/build/'+version+'/'));
+
+app.get( /^\/(.+?)\/(.*?).html$/ , function(req,res){
+    var appname = req.params[0];
+    res.render( path.join(appname) , {
+        appname:appname
+    } );
+} );
 
 var port = 9988;
 app.listen(port);
